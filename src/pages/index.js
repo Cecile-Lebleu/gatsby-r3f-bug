5230a3e3396fffe3b519c69017a3b937ba403cb4
cecile-lebleu <cecile@cecilelebleu.com>
2022-12-06 15:42:05 -0600

    Recreate bug

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,11 @@
+import { ScreenQuad } from "@react-three/drei"
+import { Canvas } from "@react-three/fiber"
 import * as React from "react"
 
 export default function Home() {
-  return <div>Hello world!</div>
+	return (
+		<Canvas style={{width: '100vw', height: '100vh'}}>
+			<ScreenQuad />
+		</Canvas>
+	)
 }
