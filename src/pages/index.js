41fec8a27da3410ce47de91bb3283084b44d926c
Cécile Lebleu <cecile@cecilelebleu.com>
2022-12-07 13:00:51 -0600

    Add plane example

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,22 @@
-import { ScreenQuad } from "@react-three/drei"
+import { OrthographicCamera, ScreenQuad } from "@react-three/drei"
 import { Canvas } from "@react-three/fiber"
 import * as React from "react"
 
 export default function Home() {
+	/// Plane example
+	
+	// return (
+	// 	<Canvas>
+	// 		<OrthographicCamera left={-1} right={1} top={1} bottom={-1} near={0} far={1} />
+	// 		<mesh>
+	// 			<planeGeometry args={[2, 2]} />
+	// 			<meshBasicMaterial color="grey" />
+	// 		</mesh>
+	// 	</Canvas>
+	// )
+		
+	/// ScreenQuad example
+
 	return (
 		<Canvas style={{width: '100vw', height: '100vh'}}>
 			<ScreenQuad />
